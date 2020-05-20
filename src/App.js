58e7c72460d44f1843f565212609270e99a141e6
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-20 15:27:07 +0530

    added dynamic data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import SearchIcon from './searchIcon';
 import './App.css';
 
 function App() {
+	const [data, setData] = useState({
+		Industries: {
+			name: 'Industries',
+			index: 0,
+			items: ['Pets & Animals', 'Professional & Commercial Services', 'Textiles & Apparel'],
+		},
+		Sectors: {
+			name: 'Sectors',
+			index: 1,
+			items: ['Agriculture', 'Chemical', 'Education'],
+		},
+		Categories: {
+			name: 'Categories',
+			index: 2,
+			items: ['Rural', 'Urban', 'Sub-urban'],
+		},
+	});
+	const [selectedCategory, setSelectedCategory] = useState('Industries');
+	const [addItem, setAddItem] = useState('');
+	const [searchKeyword, setSearchKeyword] = useState('');
+
+	const changeCategory = (item) => (event) => {
+		setSelectedCategory(item);
+	};
+
+	const handleOnChange = (item) => (event) => {
+		if (item === 'newItem') {
+			setAddItem(event.target.value);
+		}
+		if (item === 'searchItem') {
+			setSearchKeyword(event.target.value);
+		}
+	};
+
+	const addNewItem = (event) => {
+		event.preventDefault();
+
+		if (addItem === '') {
+			return alert('The Field cannot be empty');
+		}
+
+		let itemArray = data[selectedCategory].items;
+		itemArray.push(addItem);
+		setData({ ...data });
+		setAddItem('');
+	};
+
 	return (
 		<div className="App">
 			<div className="bg-card">
@@ -11,22 +58,27 @@ function App() {
 				<div className="">
 					<div className="line"></div>
 					<ul className="manage-items-list">
-						<li>
-							<button disabled>Industries</button>
-						</li>
-						<li>
-							<button>Sectors</button>
-						</li>
-						<li>
-							<button>Categories</button>
-						</li>
+						{Object.keys(data).map((item) => {
+							return (
+								<li>
+									<button onClick={changeCategory(item)} disabled={item === selectedCategory}>
+										{item}
+									</button>
+								</li>
+							);
+						})}
 					</ul>
 				</div>
 				<div className="main-card">
 					<div className="main-card-head">
-						<h4 className="main-card-heading">Industries</h4>
+						<h4 className="main-card-heading">{selectedCategory}</h4>
 						<div className="input-div">
-							<input type="text" placeholder="Search in Industries" />
+							<input
+								type="text"
+								placeholder={`Search in ${selectedCategory}`}
+								value={searchKeyword}
+								onChange={handleOnChange('searchItem')}
+							/>
 							<span>
 								<button className="search-button">
 									<SearchIcon />
@@ -36,28 +88,39 @@ function App() {
 					</div>
 					<div className="line"></div>
 					<div className="categories-table-div">
-						<div className="add-item-div">
+						<form className="add-item-div" onSubmit={addNewItem}>
 							<span>Add new item </span>
-							<input type="text" placeholder="Enter the name" />
+							<input
+								type="text"
+								value={addItem}
+								placeholder="Enter the name"
+								onChange={handleOnChange('newItem')}
+							/>
 							<button>Save</button>
-						</div>
+						</form>
 						<table className="categories-table">
 							<tbody>
-								<tr>
-									<td>
-										<div>1. {'Pets & Animals'}</div>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<div>2. {'Professional & Commercial Services'}</div>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<div>3. {'Textiles & Apparel'}</div>
-									</td>
-								</tr>
+								{data[selectedCategory].items
+									.filter((item) => item.toLowerCase().includes(searchKeyword))
+									.map((items, index) => {
+										return (
+											<tr>
+												<td>
+													<div className="item-titles">{` ${index + 1}.  ${items}`}</div>
+													<div className="buttons">
+														<button>Edit</button>
+														<button>Move</button>
+														<button>Delete</button>
+														<button
+															style={{ visibility: index === 1 ? 'visible' : 'hidden' }}
+														>
+															Conform
+														</button>
+													</div>
+												</td>
+											</tr>
+										);
+									})}
 							</tbody>
 						</table>
 					</div>
